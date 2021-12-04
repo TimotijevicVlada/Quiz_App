fa1531da963702a7eed52607a00b19d182fe9514
Vladimir <timotijevicvlada@gmail.com>
2021-12-04 20:03:45 +0100

    Working on funcionality

--- src/App.js
+++ src/App.js
@@ -1,44 +1,78 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./style/App.css";
 import Home from "./components/Home";
 import Quiz from "./components/Quiz";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-
-
   const [category, setCategory] = useState(26);
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState([]);
+  const [questionNum, setQuestionNum] = useState(1);
+  const [points, setPoints] = useState(0);
+ 
   
 
+  const fetchQuestions = useCallback(async () => {
+    const res = await fetch(
+      `https://opentdb.com/api.php?amount=1&category=${category}&difficulty=medium&type=multiple`
+    );
+    const data = await res.json();
+    console.log(data.results);
+    setQuestions(data.results);
+    const incorectAnsw = data.results[0].incorrect_answers;
+    const correctAnsw = data.results[0].correct_answer;
+    const allAnsw = [...incorectAnsw, correctAnsw];
+    setAnswers(allAnsw);
+  }, [category]);
 
-    const fetchQuestions = useCallback( async () =>{
-        const res = await fetch(`https://opentdb.com/api.php?amount=1&category=${category}&difficulty=medium&type=multiple`);
-        const data = await res.json();
-        console.log(data.results)
-        setQuestions(data.results);
-        const incorectAnsw = data.results[0].incorrect_answers;
-        const correctAnsw = data.results[0].correct_answer;
-        const allAnsw = [...incorectAnsw, correctAnsw];
-        setAnswers(allAnsw);
-        
-    }, [category])
-
-    useEffect(() => {
-        fetchQuestions();
-    }, [fetchQuestions])
+  useEffect(() => {
+    fetchQuestions();
+  }, [fetchQuestions]);
 
 
+  //Function that get question
+  const getQuestionStorage = () => {
+    if(localStorage.getItem("question") === null) {
+      localStorage.setItem("question", JSON.stringify([]));
+    } else {
+      const questionLocalStorage = JSON.parse(localStorage.getItem("question"));
+      setQuestions(questionLocalStorage);
+    }
+  }
+  useEffect(() => {
+    getQuestionStorage();
+  }, [])
 
+  //Function that save favorite item to local storage
+  const saveQuestionStorage = useCallback( () => {
+    localStorage.setItem("question", JSON.stringify(questions));
+  }, [questions])
+
+  useEffect(() => {
+    saveQuestionStorage();
+  }, [saveQuestionStorage])
 
 
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={ <Home setCategory={setCategory}/>} />
-          <Route path="/quiz" element={ <Quiz answers={answers} questions={questions}/>} />
+          <Route path="/" element={<Home setCategory={setCategory} />} />
+          <Route
+            path="/quiz"
+            element={
+              <Quiz
+                answers={answers}
+                questions={questions}
+                fetchQuestions={fetchQuestions}
+                questionNum={questionNum}
+                setQuestionNum={setQuestionNum}
+                points={points}
+                setPoints={setPoints}
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
-const Quiz = ({answers, questions}) => {
+const Quiz = ({answers, questions, fetchQuestions, setQuestionNum, questionNum, setPoints, points}) => {
+   
 
-    
+    const handleSkip = () => {
+        fetchQuestions();
+        setQuestionNum(questionNum + 1);
+        setPoints(points - 5);
+    }
 
+    const handleAnswer = (item) =>{
+        if(item === questions[0].correct_answer) {
+            setPoints(points + 10);
+        } else {
+            setPoints(points - 5);
+        }
+        setQuestionNum(questionNum + 1);
+        fetchQuestions();
+    }
 
+    const handleOrderAnswer = (item) =>{
+       if(item === 0) {
+           return "A";
+       } else if(item === 1) {
+           return "B";
+       } else if (item === 2) {
+           return "C";
+       } else {
+           return "D";
+       }
+    }
 
     return (
         <div className="quiz">
             <div className="score">
                 <div className="human_player">
-                    <span className="pts">226 pts</span>
+                    <span className="pts">{points} pts</span>
                     <span className="player_name">HUMAN PLAYER</span>
                 </div>
             </div>
@@ -20,21 +45,23 @@ const Quiz = ({answers, questions}) => {
             </div>
             <div className="quiz_content">
                 <div className="q_number">
-                    <span>QUESTION 1</span>
+                    <span>QUESTION {questionNum}</span>
                 </div>
                 <div className="question">
-                    <span>{questions[0].question}</span>
+                    {questions.map(item => (
+                        <span key={item.category}>{item.question}</span>
+                    ))}
                 </div>
                 <div className="buttons">
                     {answers.map(item => (
-                        <div>
-                            <span className="order_btn">A</span>
+                        <div onClick={() => handleAnswer(item)} key={item}>
+                            <span className="order_btn">{handleOrderAnswer(points++)}</span>
                             <span className="answer">{item}</span>
                         </div>
                     ))}
                 </div>
             </div>
-            <div className="skip">
+            <div onClick={handleSkip}  className="skip">
                 <span>SKIP</span>
             </div>
             <div className="progress_bar">
