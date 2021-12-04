a3de7df1c717d840eeabb1528ce26085ddaff549
Vladimir <timotijevicvlada@gmail.com>
2021-12-04 14:13:11 +0100

    Quiz UI done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   const [dropdown, setDropdown] = useState(false);
 
-
+  
 
   return (
     <div className="home">
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,7 +3,44 @@ import React from 'react'
 const Quiz = () => {
     return (
         <div className="quiz">
-            <h2>Hello form Quiz!</h2>
+            <div className="score">
+                <div className="human_player">
+                    <span className="pts">226 pts</span>
+                    <span className="player_name">HUMAN PLAYER</span>
+                </div>
+            </div>
+            <div className="timer">
+                <i class="far fa-clock"></i>
+                <span className="time">20</span>
+            </div>
+            <div className="quiz_content">
+                <div className="q_number">
+                    <span>QUESTION 1</span>
+                </div>
+                <div className="question">
+                    <span>Kako se zvala zena Dzona Lenona?</span>
+                </div>
+                <div className="buttons">
+                    <div>
+                        <span className="order_btn">A</span>
+                        <span className="answer">OVO ONO</span>
+                    </div>
+                    <div>
+                        <span className="order_btn">B</span>
+                        <span className="answer">JOKO ONO</span>
+                    </div>
+                    <div>
+                        <span className="order_btn">C</span>
+                        <span className="answer">ONO OVO</span>
+                    </div>
+                </div>
+            </div>
+            <div className="skip">
+                <span>SKIP</span>
+            </div>
+            <div className="progress_bar">
+                <div className="percentage"></div>
+            </div>
         </div>
     )
 }
